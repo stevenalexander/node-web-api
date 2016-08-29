ca34af3ad0c6e27ea66ded29c2afadbb6c6abbaf
Steven Alexander <steven.william.alexander@googlemail.com>
2016-08-29 09:43:44 +0100

    added simple API routes

--- app-api/routes/index.js
+++ app-api/routes/index.js
@@ -2,7 +2,19 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.json({ message: 'Hello' })
+  res.json({ items: [{ item: 'Cheese' }, { item: 'Butter' }, { item: 'Milk' }] })
+})
+
+router.get('/:id', function (req, res, next) {
+  res.json({item: 'Cheese'})
+})
+
+router.post('/', function (req, res, next) {
+  res.status(201).send({item: 'Cheese'})
+})
+
+router.delete('/:id', function (req, res, next) {
+  res.sendStatus(204)
 })
 
 module.exports = router
--- app-api/test/routes/test-route-index.js
+++ app-api/test/routes/test-route-index.js
@@ -1,9 +1,32 @@
 var supertest = require('supertest')
 var app = require('../../app')
 
-exports.root_should_return_ok = function (done) {
+exports.root_should_return_200 = function (done) {
   supertest(app)
   .get('/')
   .expect(200)
+  .end(done)
+}
+
+exports.id_should_return_200 = function (done) {
+  supertest(app)
+  .get('/1234')
+  .expect(200)
+  .end(done)
+}
+
+exports.post_should_return_204 = function (done) {
+  supertest(app)
+  .post('/')
+  .type('json')
+  .send('{"item":"Chocolate"}')
+  .expect(201)
+  .end(done)
+}
+
+exports.delete_should_return_204 = function (done) {
+  supertest(app)
+  .delete('/1234')
+  .expect(204)
   .end(done)
 }
